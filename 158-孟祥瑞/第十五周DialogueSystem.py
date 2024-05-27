0cbdc48bf04980cae38daadb5c5ac9980d82dd6a
Warrior <312652826@qq.com>
2024-05-27 12:05:37 +0800

    优化对话系统

--- 158-孟祥瑞/第十五周DialogueSystem.py
+++ 158-孟祥瑞/第十五周DialogueSystem.py
@@ -1,150 +1,157 @@
-# coding: utf-8
-"""
-基于场景脚本的对话系统
+import re
+import json
+import pandas
+
 """
 
-import re
-import pandas
-import json
+对话系统
+
+"""
 
 class DialogueSystem:
     def __init__(self):
-        self.load()     # 完成所有资源加载
+        self.load()
+        self.available_nodes = ["买衣服_node1"]
 
     def load(self):
-        self.all_nodes_info = {}    # id为key，节点信息为value
-        # 加载场景脚本
-        self.load_scenario("scenario-买衣服.json")
-        self.slot_info = {}        # id为槽位名称，value是[query, value]
-        # 加载填槽文件
-        self.load_template("slot_fitting_templet.xlsx")
+        self.all_slot_info={}
+        self.load_excel_templet("./scenario/slot_fitting_templet.xlsx")
+        self.all_node_info={}
+        self.load_scenario("./scenario/scenario-买衣服.json")
 
-    def load_scenario(self, file_path):
-        scenario_name = file_path.replace(".json", "")
-        with open(file_path, "r", encoding="utf-8") as f:
-            for node_info in json.load(f):
-                node_id = node_info["id"]
-                node_id = scenario_name + "_" + node_id
+    def load_excel_templet(self,path):
+        df = pandas.read_excel(path)
+        for i in range(len(df)):
+            slot = df['slot'][i]
+            query = df['query'][i]
+            values = df['values'][i]
+            # 问题和 槽位信息
+            self.all_slot_info[slot] = [query, values]
+
+    def load_scenario(self,path):
+        # 场景名称
+        scen_name = path.split('/')[-1].split("-")[1].split('.')[0]
+        with open(path,encoding='utf-8') as f:
+            # 读成python的数据结构
+            data = json.loads(f.read())
+            for node_info in data:
+                node_name = node_info["id"]
+                # 判断node_info 中是否有childnode ，字典可以直接询问是否存在键，但无法直接访问是否存在某个值
                 if "childnode" in node_info:
-                    node_info["childnode"] = [scenario_name + "_" + child_node_id for child_node_id in node_info["childnode"]]
-                self.all_nodes_info[node_id] = node_info
-        # print(self.all_nodes_info)
-        return
+                    node_info["childnode"] = [scen_name +"_"+ name for name in node_info["childnode"]]
+                node_name = scen_name + "_" + node_name
+                self.all_node_info[node_name] = node_info
 
-    def load_template(self, file_path):
-        self.slot_template = pandas.read_excel(file_path, sheet_name="Sheet1")
-        for index, row in self.slot_template.iterrows():
-            self.slot_info[row["slot"]] = [row["query"], row["values"]]
-        # print("slot info: \n", self.slot_info)
-        return
+    def get_intent(self,memory):
+        score = -1
+        hit_node =None
+        for node in self.available_nodes:
+            intent_score = self.get_intent_score(memory,node)
+            if intent_score >score:
+                score =intent_score
+                hit_node = node
 
-    def run(self, user_query, memory):
-        if len(memory) == 0:
-            memory["available_nodes"] = ["scenario-买衣服_node1"]
-        memory["user_query"] = user_query
+        memory["score"] = score
+        memory["hit_node"] = hit_node
+        return memory
+
+    # 节点意图识别
+    def get_intent_score(self,memory,node):
+        scores=[]
+        for intent in self.all_node_info[node]["intent"]:
+            score = self.sentence_match(intent,memory["query"])
+            scores.append(score)
+        return max(scores)
+
+    #  文本匹配算法
+    def sentence_match(self,intent,query):
+        return  len(set(intent)&set(query)) / len(set(intent)|set(query))
+
+    # 槽位抽取
+    def get_slot(self,memory):
+        # 根据获得的节点，遍历槽位信息
+        for slot in self.all_node_info[memory["hit_node"]].get("slot",[]):
+            # 获得槽位
+            pattern = self.all_slot_info[slot][1]
+            # 搜索到一个就结束，re.search()方法扫描整个字符串，并返回第一个成功的匹配。如果匹配失败，则返回None。
+            match = re.search(pattern, memory["query"])
+
+            if match is not None:
+                memory[slot] =  match.group()
+        return memory
+
+    def nlu(self,memory):
+        memory = self.get_intent(memory)
+        memory = self.get_slot(memory)
+        return memory
+
+    # 判断当前的槽位是否填满
+    def dst(self,memory):
+        for slot in self.all_node_info[memory["hit_node"]].get("slot", []):
+            if slot not in memory:
+                memory["require_slot"] = slot
+                return memory
+
+        memory["require_slot"] = None
+        return memory
+
+    def take_action(self,memory):
+        return memory
+
+    # 策略选择
+    def pm(self,memory):
+        if memory["require_slot"] is None:
+            memory["policy"] = "reply"
+            memory = self.take_action(memory)
+            self.available_nodes = self.all_node_info[memory["hit_node"]].get("childnode")
+        else:
+            memory["policy"] = "ask"
+            self.available_nodes = [memory["hit_node"]]
+        return memory
+
+    def nlg(self,memory):
+        if memory["policy"] == "reply":
+            response = self.all_node_info[memory["hit_node"]].get("response")
+            response = self.fill_templet(memory,response)
+        else:
+            slot = memory["require_slot"]
+            response = self.all_slot_info[slot][0]
+
+        memory["response"] = response
+        return memory
+
+    def fill_templet(self,memory,response):
+        for slot in self.all_node_info[memory["hit_node"]].get("slot",[]):
+            response = re.sub(slot, memory[slot],response)
+        return response
+
+    def run(self,query,memory):
+
+        memory["query"] = query
+        # 自然语言理解
         memory = self.nlu(memory)
+        # 对话状态的跟踪  dialogue state tracking
         memory = self.dst(memory)
-        memory = self.dpo(memory)
+        # 策略选择 policy making
+        memory = self.pm(memory)
+        # 自然语言生成
         memory = self.nlg(memory)
         return memory
 
-    def nlu(self, memory):
-        # 意图识别 + 槽位抽取
-        memory = self.intent_recognition(memory)
-        memory = self.slot_filling(memory)
-        return memory
 
-    def intent_recognition(self, memory):
-        # 意图识别
-        hit_score = -1
-        hit_node_id = None
-        for node_id in memory["available_nodes"]:      # 当前可访问节点
-            node_info = self.all_nodes_info[node_id]
-            score = self.calc_intent_score(memory, node_info)
-            if score > hit_score:
-                hit_score = score
-                hit_node_id = node_id
-        memory["hit_node_id"] = hit_node_id
-        memory["hit_score"] = hit_score
-        return memory
+if __name__ =="__main___":
+    dl = DialogueSystem()
+    query = "我想买衣服"
 
-    def calc_intent_score(self, memory, node_info):
-        user_query = memory["user_query"]
-        intent_list = node_info["intent"]
-        all_scores = []
-        for intent in intent_list:
-            score = self.sentence_similarity(user_query, intent)
-            all_scores.append(score)
-        return max(all_scores)
-
-    def sentence_similarity(self, sentence1, sentence2):
-        return len(set(sentence1) & set(sentence2)) / len(set(sentence1) | set(sentence2))
-
-    def slot_filling(self, memory):
-        # 槽位抽取
-        user_query = memory["user_query"]
-        slot_list = self.all_nodes_info[memory["hit_node_id"]].get("slot", [])
-        for slot in slot_list:
-            # print("slot is: ", slot)
-            _, candidates = self.slot_info[slot]
-            # print(candidates, user_query)
-            search_result = re.search(candidates, user_query)
-            if search_result is not None:
-                memory[slot] = search_result.group()
-        # print("memory: ", memory)
-        return memory
-
-    def dst(self, memory):
-        # 对话状态跟踪，检查命中节点对应的槽位信息是否齐备
-        for slot in self.all_nodes_info[memory["hit_node_id"]].get("slot", []):
-            # print(self.all_nodes_info)
-            if slot not in memory or memory[slot] == "":
-                memory["missing_slot"] = slot
-                return memory
-        memory["missing_slot"] = None
-        return memory
-
-    def dpo(self, memory):
-        # 如果缺少槽位：反问；如果不缺槽位：回答
-        if memory["missing_slot"] is not None:
-            slot = memory["missing_slot"]
-            memory["policy"] = "ask"
-            memory["available_nodes"] = [memory["hit_node_id"]]
-        else:
-            memory["policy"] = "answer"
-            memory["available_nodes"] = self.all_nodes_info[memory["hit_node_id"]].get("childnode", [])
-        return memory
-
-    def nlg(self, memory):
-        # 根据policy做选择
-        if memory["policy"] == "ask":
-            # 找到missing_slot
-            slot = memory["missing_slot"]
-            ask_sentence, _ = self.slot_info[slot]
-            memory["response"] = ask_sentence
-        elif memory["policy"] == "answer":
-            # 根据命中节点找response
-            response = self.all_nodes_info[memory["hit_node_id"]]["response"]
-            # 把槽位替换成memory中真实值
-            response = self.replace_slot_in_response(memory, response)
-            memory["response"] = response
-        return memory
-
-    def replace_slot_in_response(self, memory, response):
-        # 把response中的槽位替换成memory中真实值
-        # 找到命中节点的槽位
-        slots = self.all_nodes_info[memory["hit_node_id"]].get("slot", [])
-        for slot in slots:
-            response = re.sub(slot, memory[slot], response)
-        return response
-
-
-if __name__ == '__main__':
-    dialogue_system = DialogueSystem()
-    memory = {}  # 对话状态跟踪
-    # print(memory)
+    print(dl.all_node_info)
+    memory={}
+    print(dl.all_slot_info)
     while True:
-        user_query = input("请输入：")
-        dialogue_system.run(user_query, memory)
-        print("回答：", memory["response"])
+        query = input("用户输入(按q退出）: ")
+        if query =="q" or query in "再见":
+            print("系统回答：谢谢光临")
+            break
+        memory = dl.run(query,memory)
         print(memory)
+        print("系统回答：",memory["response"])
+        print()
